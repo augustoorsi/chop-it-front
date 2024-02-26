4e5d5835367a38481caab44645a495a6eb2e7bbf
augusto <augusford@hotmail.com>
2024-02-26 12:11:59 -0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return(
         <div>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/*" element={<Home/>}/>
             </Routes>
         </div>
     )
